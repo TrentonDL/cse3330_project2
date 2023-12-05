11728aec422397c45c45f6d6cda441bf98eb3c8c
Joel Guerra <joel.guerra@mavs.uta.edu>
2023-12-05 02:42:28 -0600

    finished code

--- Part_3/task2Joel.py
+++ Part_3/task2Joel.py
@@ -1,21 +1,24 @@
 from tkinter import *
 from datetime import date, timedelta
 import prettytable
-
+from tkcalendar import DateEntry
 import sqlite3
+import locale
 
-import os
-
-def getcwd(fname):
-    cwd = os.path.dirname(__file__)                                                 # get the current working directory
-    path = os.path.abspath(os.path.join(cwd,fname))                                 # get the real path of the filename and join with the cwd
-    return path
 
 # THIS IS A PLACEHOLDER FUNCTION
 
 def button_click():
-    # Add the functionality you want when a button is clicked
-    print("Button clicked!")
+    
+    pop_window = Toplevel(root)
+    pop_window.title("TEST")
+    pop_window.geometry("200x100")
+
+    label = Label(pop_window, text="Just a quick test!")
+    label.pack(pady=10)
+
+    pop_window.grab_set()
+    root.wait_window(pop_window) 
 
 def checkout_book():
     def finalize_checkout():
@@ -52,7 +55,7 @@ def checkout_book():
         updated_copies_window.title("Updated Books Copies")
         updated_copies_window.geometry("720x360")
 
-        conne = sqlite3.connect(getcwd("LMS.db"))
+        conne = sqlite3.connect('LMS.db')
         cursor = conne.cursor()
 
 
@@ -77,7 +80,7 @@ def checkout_book():
     checkout_window.title("Check-Out a Book!")
     checkout_window.geometry("300x325")
 
-    conn = sqlite3.connect(getcwd('LMS.db'))
+    conn = sqlite3.connect('LMS.db')
     curs = conn.cursor()
 
     curs.execute("SELECT Title FROM BOOK")
@@ -129,7 +132,7 @@ def checkout_book():
 def add_new_user():
 
     def insert_new_user():
-        conn = sqlite3.connect(getcwd("LMS.db"))
+        conn = sqlite3.connect('LMS.db')
         cursor = conn.cursor()
 
         new_name = full_name.get()
@@ -192,7 +195,7 @@ def add_new_user():
 def add_books():
 
     def insert_book():
-        conn = sqlite3.connect(getcwd("LMS.db"))
+        conn = sqlite3.connect('LMS.db')
         cursor = conn.cursor()
 
         new_book = book_title.get()
@@ -239,7 +242,7 @@ def add_books():
     book_title = Entry(add_book_window, width = 30)
     book_title.grid(row = 0, column = 1, padx = 20)
 
-    conn = sqlite3.connect(getcwd("LMS.db"))
+    conn = sqlite3.connect('LMS.db')
     cursor = conn.cursor()
 
     cursor.execute("SELECT Publisher_Name FROM PUBLISHER")
@@ -305,7 +308,7 @@ def loan_records_by_book():
     label1 = Label(select_book_window, text="Select a book to search loan records: ")
     label1.pack(pady=10)
 
-    conn = sqlite3.connect(getcwd("LMS.db"))
+    conn = sqlite3.connect('LMS.db')
     curs = conn.cursor()
 
     curs.execute("SELECT Title FROM BOOK")
@@ -324,6 +327,243 @@ def loan_records_by_book():
     select_book_window.grab_set()
     root.wait_window(select_book_window)
 
+def late_report_by_range():
+
+    def search_with_dates():
+
+        conn = sqlite3.connect('LMS.db')
+        cursor = conn.cursor()
+
+        date1 = date_entry1.get_date()
+        date1_formatted = date1.strftime('%Y-%m-%d')
+        date2 = date_entry2.get_date()
+        date2_formatted = date2.strftime('%Y-%m-%d')
+
+        query = "SELECT Book_Id, Branch_Id, Card_No, Date_Out, Due_Date, Returned_date, CAST(julianday(Returned_date) - julianday(Due_Date) AS INTEGER) FROM BOOK_LOANS WHERE Returned_date IS NOT NULL AND Returned_date > Due_Date AND (Due_Date BETWEEN ? AND ?)"
+        cursor.execute(query, (date1_formatted, date2_formatted, ))
+
+        query_result = cursor.fetchall()
+
+        select_range_window.destroy()
+
+        latebooks_win = Toplevel(root)
+        latebooks_win.title("Books Returned Late Between Given Dates")
+        latebooks_win.geometry("715x225")
+
+        table = prettytable.PrettyTable(['Book ID', 'Branch ID', 'Card Number', 'Date Checked Out', 'Due Date', 'Returned Date', 'Days Late'])
+
+        for row in query_result:
+            table.add_row(row)
+
+        text_widget = Text(latebooks_win, wrap=NONE, width=600, height=80)
+        text_widget.insert(END, table.get_string())
+        text_widget.pack(padx=10, pady=10)
+
+        conn.close()
+
+    select_range_window = Toplevel(root)
+    select_range_window.title("Find Late Returns")
+    select_range_window.geometry("300x200")
+
+    label1 = Label(select_range_window, text="Please Select Two Dates: ")
+    label1.pack(pady=10)
+
+    date_entry1 = DateEntry(select_range_window, width=12, borderwidth=2)
+    date_entry1.pack(pady=5)
+
+    date_entry2 = DateEntry(select_range_window, width=12, borderwidth=2)
+    date_entry2.pack(pady=5)
+
+    # Add a button to trigger the search function
+    search_button = Button(select_range_window, text="Search", command=search_with_dates)
+    search_button.pack(pady=10)
+
+    select_range_window.grab_set()
+    root.wait_window(select_range_window)
+
+def view_options():
+
+    def borrower_report():
+
+        def generate_b_report():
+
+            conn = sqlite3.connect('LMS.db')
+            cursor = conn.cursor()
+
+            borrower_id_input = borrower_id.get()
+            borrower_name_input = borrower_name.get()
+
+            borrower_id_value = int(borrower_id_input) if borrower_id_input else None
+
+            query = "select Card_No, Name, LateFeeBalance from vBookLoanInfo where 1+1"
+
+            if borrower_id_value is not None:
+                query += f" AND Card_No = {borrower_id_value}"
+
+            if borrower_name_input is not None:
+                query += f" AND Name LIKE '%{borrower_name_input}%'"
+
+            query += f"order by LateFeeBalance Desc"
+
+            cursor.execute(query)
+
+            results = cursor.fetchall()
+
+            borrower_search_win.destroy()
+
+            display_breport_win = Toplevel(root)
+            display_breport_win.title("Borrower Late Fee Report")
+            display_breport_win.geometry("400x200")
+
+            locale.setlocale(locale.LC_ALL, '')
+
+            table = prettytable.PrettyTable(['Borrower ID', 'Name', 'Late Fee Balance'])
+
+            for row in results:
+
+                late_fee_balance = locale.currency(row[2], grouping=True)
+
+                table.add_row([row[0], row[1], late_fee_balance])
+
+            text_widget = Text(display_breport_win, wrap=NONE, width=600, height=80)
+            text_widget.insert(END, table.get_string())
+            text_widget.pack(padx=10, pady=10)
+
+            conn.close()
+
+        view_option_win.destroy()
+
+        borrower_search_win = Toplevel(root)
+        borrower_search_win.title("Borrower Report")
+        borrower_search_win.geometry("425x150")
+
+        label1 = Label(borrower_search_win, text = "Search by name or borrower ID or leave blank.")
+        label1.grid(row = 0, column = 1)
+
+        borrower_id = Entry(borrower_search_win, width = 30)
+        borrower_id.grid(row = 1, column = 1, padx = 20)
+
+        borrower_name = Entry(borrower_search_win, width = 30)
+        borrower_name.grid(row = 2, column = 1)
+
+        id_label = Label(borrower_search_win, text = 'Borrower ID: ')
+        id_label.grid(row = 1, column = 0)
+
+        name_label = Label(borrower_search_win, text = 'Borrower Name: ')
+        name_label.grid(row = 2, column = 0)
+
+        confirm_button = Button(borrower_search_win, text="Search", command=generate_b_report)
+        confirm_button.grid(row = 3, column = 1, padx=10, pady=20)
+
+        borrower_search_win.grab_set()
+        root.wait_window(borrower_search_win)
+
+    def book_report():
+
+        def generate_book_report():
+
+            conn = sqlite3.connect('LMS.db')
+            cursor = conn.cursor()
+
+            borrower_id_input = borrower_id.get()
+            book_id_input = book_id.get()
+            book_title_input = book_title.get()
+
+            book_id_value = int(book_id_input) if book_id_input else None
+
+            query = "SELECT VB.Card_No, B.Book_Id, B.Title, B.Publisher_Name, VB.LateFeeBalance FROM BOOK AS B JOIN vBookLoanInfo AS VB ON B.Title=VB.Title where VB.Card_No = ?"
+
+            if book_id_value is not None:
+                query += f" AND B.Book_Id = {book_id_value}"
+
+            if book_title_input is not None:
+                query += f" AND B.Title LIKE '%{book_title_input}%'"
+
+            query += f" order by VB.LateFeeBalance Desc"
+
+            cursor.execute(query, (borrower_id_input, ))
+
+            results = cursor.fetchall()
+
+            book_search_win.destroy()
+
+            display_book_report_win = Toplevel(root)
+            display_book_report_win.title("Late Fee Report With Book Information")
+            display_book_report_win.geometry("640x150")
+
+            locale.setlocale(locale.LC_ALL, '')
+
+            table = prettytable.PrettyTable(['Borrower ID', 'Book ID', 'Book Title', 'Publlisher', 'Late Fee Balance'])
+
+            for row in results:
+
+                late_fee_balance = locale.currency(row[4], grouping=True) if row[4] != 0 else "Non-Applicable"
+
+                table.add_row([row[0], row[1], row[2], row[3], late_fee_balance])
+
+            text_widget = Text(display_book_report_win, wrap=NONE, width=600, height=80)
+            text_widget.insert(END, table.get_string())
+            text_widget.pack(padx=10, pady=10)
+
+
+            conn.close()
+
+
+        view_option_win.destroy()
+
+        book_search_win = Toplevel(root)
+        book_search_win.title("Borrower Report")
+        book_search_win.geometry("520x175")
+
+        label1 = Label(book_search_win, text = "Enter Borrower ID, then may enter Book ID, Book Title or neither one.")
+        label1.grid(row = 0, column = 1)
+
+        borrower_id = Entry(book_search_win, width = 30)
+        borrower_id.grid(row = 1, column = 1, padx = 20)
+
+        book_id = Entry(book_search_win, width = 30)
+        book_id.grid(row = 2, column = 1)
+
+        book_title = Entry(book_search_win, width = 30)
+        book_title.grid(row = 3, column = 1)
+
+        id_label = Label(book_search_win, text = 'Borrower ID: ')
+        id_label.grid(row = 1, column = 0)
+
+        book_id_label = Label(book_search_win, text = 'Book ID: ')
+        book_id_label.grid(row = 2, column = 0)
+
+        book_title_label = Label(book_search_win, text = 'Book Title: ')
+        book_title_label.grid(row = 3, column = 0)
+
+        confirm_button = Button(book_search_win, text="Search", command=generate_book_report)
+        confirm_button.grid(row = 4, column = 1, padx=10, pady=20)
+
+        book_search_win.grab_set()
+        root.wait_window(book_search_win)
+
+
+    view_option_win = Toplevel(root)
+    view_option_win.title("View Options")
+    view_option_win.geometry("300x75")
+
+    view_option_win.columnconfigure(0, weight=1)
+    view_option_win.columnconfigure(1, weight=1)
+    view_option_win.rowconfigure(0, weight=1)
+
+    button1 = Button(view_option_win, text="Borrower Report", pady=5, command=borrower_report)
+    button2 = Button(view_option_win, text="Book Report", pady=5, command=book_report)
+
+    button1.grid(row=0, column=0, sticky="nsew")
+    button2.grid(row=0, column=1, sticky="nsew")
+
+    view_option_win.rowconfigure(1, weight=1)
+    view_option_win.rowconfigure(2, weight=1)
+
+
+    view_option_win.grab_set()
+    root.wait_window(view_option_win)
+
 
 
 #create Tkinter Window
@@ -340,8 +580,8 @@ button1 = Button(root, text="Check-Out A Book", command=checkout_book)
 button2 = Button(root, text="Add A New User", command=add_new_user)
 button3 = Button(root, text="Add A New Book", command=add_books)
 button4 = Button(root, text="Loan Records By Book Title", command=loan_records_by_book)
-button5 = Button(root, text="Find Late Books By Due Date", command=button_click)
-button6 = Button(root, text="Book Loan Information", command=button_click)
+button5 = Button(root, text="Find Late Books By Date Range", command=late_report_by_range)
+button6 = Button(root, text="Book Loan Information", command=view_options)
 
 # Use the grid manager to arrange the buttons in a grid layout
 button1.grid(row=0, column=0, sticky="nsew")
